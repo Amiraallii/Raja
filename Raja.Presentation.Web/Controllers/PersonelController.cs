b264d341fe8327d598e401723b2f73263d22edca
Amiraallii <129048287+Amiraallii@users.noreply.github.com>
2025-04-19 00:50:04 +0330

    comit 2

--- Raja.Presentation.Web/Controllers/PersonelController.cs
+++ Raja.Presentation.Web/Controllers/PersonelController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Raja.Application.Contract.Dto;
 using Raja.Application.Contract.IServices;
 using Raja.Application.Services;
@@ -28,7 +29,37 @@ namespace Raja.Presentation.Web.Controllers
                 return Ok(result);
             }
 
+            [HttpPost]
+            public async Task<IActionResult> Add(PersonelDto personel)
+            {
+                if (!ModelState.IsValid)
+                    return RedirectToAction(nameof(Index), personel);
 
+                await _personnelService.Add(personel);
+                return RedirectToAction(nameof(Index));
+            }
+
+            [HttpPost]
+            public async Task<IActionResult> Edit(PersonelDto personel, CancellationToken ct)
+            {
+                if (!ModelState.IsValid)
+                    return RedirectToAction(nameof(Index), personel);
+
+                await _personnelService.Edit(personel, ct);
+                return RedirectToAction(nameof(Index));
+            }
+
+            public async Task<IActionResult> Remove(int id)
+            {
+                await _personnelService.Remove(id);
+                return RedirectToAction(nameof(Index));
+            }
+
+            public async Task<IActionResult> Detail(int id, CancellationToken ct)
+            {
+                var personel = await _personnelService.Get(id, ct);
+                return RedirectToAction(nameof(Index), personel);
+            }
         }
     }
 }
